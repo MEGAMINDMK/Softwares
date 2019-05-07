213d209c12d505b428a2b382c4e13b5e1c0d1ef5
webnet <muhommadraahim1@gmail.com>
2019-05-07 23:16:34 +0500

    Delete wnobnp.bat

--- cpp source/Booster/bin/Debug/wnobnp.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Webnet Official Booster
-echo Enter Name of "program.exe" to Un Boost, else close the window
-:start
-set /P "Name="
-wmic process where name="%Name%" CALL setpriority "normal"
